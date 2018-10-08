994b3eabb8b0938e8f7ee35a4aafcff403527eeb
andrew789 <yushao.cheng@gmail.com>
2018-10-08 17:38:02 +0800

    .

--- aln2fas.pl
+++ aln2fas.pl
@@ -1,62 +1,62 @@
 #!/usr/bin/perl
 
-#check command-line arguments
+# check command-line arguments
 if (@ARGV != 2) {
 	printf STDERR "aln2fas.pl <arg1> <arg2>\n";
 	printf STDERR "<arg1>, clustalw alignment file\n";
 	printf STDERR "<arg2>, fasta-format sequence file\n";
 	exit;
 }
-#check if clustalw alignment file exists
+# check if clustalw alignment file exists
 if (not -f $ARGV[0]) {
 	printf STDERR "aln2fas.pl error, $ARGV[0] not found\n";
 	exit;
 }
-#open clustalw alignment file
+# open clustalw alignment file
 open(ALN, $ARGV[0]);
 @alntxt = <ALN>;
 $alnlen = @alntxt;
 close(ALN);
-#check the title of clustalw alignment file
+# check the title of clustalw alignment file
 if ($alntxt[0] !~ /CLUSTAL/) {
 	printf STDERR "aln2fas.pl error, title line not found in alignment\n";
 	exit;
 }
-#read in the clustalw alignment
+# read in the clustalw alignment
 for ($nbloc = 0, $i = 1; $i < $alnlen; $i++) {
 	chomp $alntxt[$i];
-	#multiple-alignment block starts with a non-blank & non-star line
+	# multiple-alignment block starts with a non-blank & non-star line
 	if (length($alntxt[$i]) != 0 and substr($alntxt[$i], 0, 1) ne " ") {
 		for ($nseq = 0, $j = $i; $j < $alnlen; $j++) {
 			chomp $alntxt[$j];
-			#multiple-alignment block stops with a blank or star line
+			# multiple-alignment block stops with a blank or star line
 			if (length($alntxt[$j]) == 0 or substr($alntxt[$j], 0, 1) eq " ") {
 				$i = $j;
 				last;
 			}
-			#split the string
+			# split the string
 			@arr = split / +/, $alntxt[$j];
-			#sequence name
+			# sequence name
 			$seqname[$nseq] = $arr[0];
-			#alignment segment
+			# alignment segment
 			$alnbloc[$nbloc][$nseq] = $arr[1];
-			#sequence counter
+			# sequence counter
 			$nseq++;
 		}
-		#alignment block counter
+		# alignment block counter
 		$nbloc++;
 	}
 }
-#build alignment array
+# build alignment array
 for ($i = 0; $i < $nseq; $i++) {
-	#reconstruct alignment
+	# reconstruct alignment
 	$alignment[$i] = $alnbloc[0][$i];
-	#attach pieces from dif ferent blocks
+	# attach pieces from dif ferent blocks
 	for ($j = 1; $j < $nbloc; $j++) {
 		$alignment[$i] = $alignment[$i].$alnbloc[$j][$i];
 	}
 }
-#output fasta-format sequence file
+# output fasta-format sequence file
 open(NEW, ">$ARGV[1]");
 for ($i = 0; $i < $nseq; $i++) {
 	printf NEW (">$seqname[$i]\n");
--- aln2sto.pl
+++ aln2sto.pl
@@ -1,60 +1,60 @@
 #!/usr/bin/perl
 
-#check command-line arguments
+# check command-line arguments
 if (@ARGV != 2) {
 	printf STDERR "aln2sto.pl <arg1> <arg2>\n";
-	printf STDERR "<arg1>: clustalw alignment file\n";
-	printf STDERR "<arg2>: stockholm1.0 alignment file\n";
+	printf STDERR "<arg1>, clustalw alignment file\n";
+	printf STDERR "<arg2>, stockholm1.0 alignment file\n";
 	exit;
 }
-#check if clustalw alignment file exists
+# check if clustalw alignment file exists
 if (not -f $ARGV[0]) {
-	printf STDERR "aln2sto.pl error: $ARGV[0] not found\n";
+	printf STDERR "aln2sto.pl error, $ARGV[0] not found\n";
 	exit;
 }
-#open clustalw alignment file
+# open clustalw alignment file
 open(ALN, $ARGV[0]);
 @alntxt = <ALN>;
 $alnlen = @alntxt;
 close(ALN);
-#check the title of clustalw alignment file
+# check the title of clustalw alignment file
 if ($alntxt[0] !~ /CLUSTAL/) {
-	printf STDERR "aln2sto.pl error: title line not found in alignment\n";
+	printf STDERR "aln2sto.pl error, title line not found in alignment\n";
 	exit;
 }
-#read in the clustalw alignment
+# read in the clustalw alignment
 for ($nbloc = 0, $i = 1; $i < $alnlen; $i++) {
 	chomp $alntxt[$i];
-	#multiple-alignment block starts with a non-blank & non-star line
+	# multiple-alignment block starts with a non-blank & non-star line
 	if (length($alntxt[$i]) != 0 and substr($alntxt[$i], 0, 1) ne " ") {
 		for ($nseq = 0, $j = $i; $j < $alnlen; $j++) {
 			chomp $alntxt[$j];
-			#multiple-alignment block stops with a blank or star line
+			# multiple-alignment block stops with a blank or star line
 			if (length($alntxt[$j]) == 0 or substr($alntxt[$j], 0, 1) eq " ") {
 				$i = $j;
 				last;
 			}
-			#split the string
+			# split the string
 			@arr = split / +/, $alntxt[$j];
-			#sequence name
+			# sequence name
 			$seqname[$nseq] = $arr[0];
-			#alignment segment
+			# alignment segment
 			$alnbloc[$nbloc][$nseq] = $arr[1];
-			#sequence counter
+			# sequence counter
 			$nseq++;
 		}
-		#alignment block counter
+		# alignment block counter
 		$nbloc++;
 	}
 }
-#build stockholm1.0 alignment
+# build stockholm1.0 alignment
 for ($i = 0; $i < $nseq; $i++) {
-	#reconstruct alignment
+	# reconstruct alignment
 	$alignment[$i] = $alnbloc[0][$i];
 	for ($j = 1; $j < $nbloc; $j++) {
 		$alignment[$i] = $alignment[$i].$alnbloc[$j][$i];
 	}
-	#alignment length
+	# alignment length
 	$strlen = length($alignment[$i]);
 	for ($j = 0; $j < $strlen; $j++) {
 		if (substr($alignment[$i], $j, 1) eq "-") {
@@ -62,15 +62,15 @@ for ($i = 0; $i < $nseq; $i++) {
 		}
 	}
 }
-#maximal length of sequence name
+# maximal length of sequence name
 for ($maxnmlen = -100, $i = 0; $i < $nseq; $i++) {
 	if (length($seqname[$i]) > $maxnmlen) {
 		$maxnmlen = length($seqname[$i]);
 	}
 }
-#output stockholm1.0 alignment
+# output stockholm1.0 alignment
 open(STO, ">$ARGV[1]");
-printf STO "#stockholm1.0\n\n";
+printf STO "# stockholm1.0\n\n";
 for ($i = 0; $i < $nseq; $i++) {
 	if ($maxnmlen <= 5) {
 		printf STO ("%-6s", $seqname[$i]);
@@ -94,7 +94,7 @@ for ($i = 0; $i < $nseq; $i++) {
 		printf STO ("%-36s", $seqname[$i]);
 	}
 	else {
-		printf STDERR "aln2sto.pl error: sequence name too long\n";
+		printf STDERR "aln2sto.pl error, sequence name too long\n";
 		exit;
 	}
 	printf STO "$alignment[$i]\n";
